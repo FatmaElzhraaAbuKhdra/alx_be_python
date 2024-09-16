310bb91285441e5ab28c4c29cf6d8c05c70386ea
fatma105 <fatmaabukhdra1361998@gmail.com>
2024-09-16 17:54:58 +0300

    update

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,19 +1,7 @@
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
+# match_case_calculator.py
 
-# نحاول تحويل المدخلات إلى أرقام صحيحة
-try:
-    num1 = int(num1)
-    num2 = int(num2)
-except ValueError:
-    print("Error: Invalid number entered. Please enter valid integers.")
-    exit()
-
-<<<<<<< HEAD
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-=======
->>>>>>> 72b6fe34fe2f9441e975aeebdb82b174b9ac51f7
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 operator = input("Choose the operation (+, -, *, /): ")
 
 match operator:
@@ -34,4 +22,3 @@ match operator:
             print(f"The result is {result}.")
     case _:
         print("Error: Invalid operation. Please enter +, -, *, or /.")
-
