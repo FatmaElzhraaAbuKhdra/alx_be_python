1998ad261aa5a160c4804fdb25e77a6d6660013d
fatma105 <fatmaabukhdra1361998@gmail.com>
2024-09-25 03:40:15 +0300

    update

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,8 +13,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            print("Enter the name of the item to add:['\"]")
-            item=input().strip()
+            item = input("Enter the name of the item to add: ").strip()
             shopping_list.append(item)
         elif choice == '2':
             # Prompt for and remove an item
